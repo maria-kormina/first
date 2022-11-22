27041146839d789ac62f699148df38f51e1c482e
Yuriy Kormin <yuriy.kormin@gmail.com>
2022-11-22 10:53:50 +0400

    add lesson

---
+++ списки/01/exercise/exercise.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def get_square_roots(digit):
+    pass
+
+
+def test_():
+    assert get_square_roots(-1) == []
+    assert get_square_roots(0) == [0]
+    assert get_square_roots(25) == [-5.0, 5.0]
